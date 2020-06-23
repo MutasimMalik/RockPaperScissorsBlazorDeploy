09b45f105c3419eb609fd618eb54f1b6d2ea9d9d
Confusing21 <malikmutasim2121@gmail.com>
2020-06-23 21:17:38 +0600

    Working version

---
+++ Client/Helpers/GameStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorBlazorV2.Client.Helpers
+{
+    public enum GameStatus
+    {
+        Victory, Loss, Draw
+    }
+}
